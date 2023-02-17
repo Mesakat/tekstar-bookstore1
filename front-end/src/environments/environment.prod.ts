6f77309c55079e7cb52d8cbc6f5277a36d3d146a
Sanxpros <110634108+Sanxpros@users.noreply.github.com>
2023-02-17 15:53:47 -0600

    Update environment.prod.ts

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  resourceApi: "https://api.mariomerco.com/resources-api",
-  inventoryApi: "https://api.mariomerco.com/inventory-api",
-  clientApi: "https://api.mariomerco.com/clients-api",
-  rentingApi: "https://api.mariomerco.com/renting-api" 
+  resourceApi: "https://api.tekstarjobs.com/resources-api",
+  inventoryApi: "https://api.tekstarjobs.com/inventory-api",
+  clientApi: "https://api.tekstarjobs.com/clients-api",
+  rentingApi: "https://api.tekstarjobs.com/renting-api" 
 };
